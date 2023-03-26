f258ff3a3e7431e7ff4625c047e57515870a206e
System Malt <javierorfo@protonmail.com>
2023-03-26 12:39:57 -0300

    Update ron.lua

--- ftdetect/ron.lua
+++ ftdetect/ron.lua
@@ -1,6 +1,6 @@
--- #########################################################
--- # Maintainer: Javier Orfo                               #
--- # URL:        https://github.com/javiorfo/nvim-nyctovim #
--- #########################################################
+-- ###########################################################
+-- # Maintainer: System Malt                                 #
+-- # URL:        https://github.com/systemmalt/nvim-nyctovim #
+-- ###########################################################
 
 vim.cmd[[au BufRead,BufNewFile *.ron set filetype=ron]]
