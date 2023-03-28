61b95a95ffea7b71804993e740daa16e7ed9cb5e
Javier Orfo <javierorfo@protonmail.com>
2023-03-28 00:19:45 -0300

    Update javascript.lua

--- lua/nyctovim/syntax/javascript.lua
+++ lua/nyctovim/syntax/javascript.lua
@@ -2,6 +2,7 @@
 -- # Maintainer: Javier Orfo                               #
 -- # URL:        https://github.com/javiorfo/nvim-nyctovim #
 -- #########################################################
+
 local linkAll = require'nyctovim.util'.linkAll
 
 linkAll {
