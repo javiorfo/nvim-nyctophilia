468ff5074f4271c660891d1a6b57f9e400f19237
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:43:37 -0300

    Update ron.lua

--- ftdetect/ron.lua
+++ ftdetect/ron.lua
@@ -1,6 +1,1 @@
--- ########################################################
--- # Maintainer: Mr. Charkuils                            #
--- # URL:        https://github.com/charkuils/nvim-whisky #
--- ########################################################
-
 vim.cmd[[au BufRead,BufNewFile *.ron set filetype=ron]]
