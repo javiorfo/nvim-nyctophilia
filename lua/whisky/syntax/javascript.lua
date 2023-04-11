7cb2208e42a47efba9c459c5cce12a4c2f9195eb
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:45:09 -0300

    Update javascript.lua

--- lua/whisky/syntax/javascript.lua
+++ lua/whisky/syntax/javascript.lua
@@ -1,8 +1,3 @@
--- ########################################################
--- # Maintainer: Mr. Charkuils                            #
--- # URL:        https://github.com/charkuils/nvim-whisky #
--- ########################################################
-
 local linkAll = require'whisky.util'.linkAll
 
 linkAll {
