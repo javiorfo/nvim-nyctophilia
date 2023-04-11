d7e0113b35766578955c4450e281c8af0ce41e87
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:45:16 -0300

    Update json.lua

--- lua/whisky/syntax/json.lua
+++ lua/whisky/syntax/json.lua
@@ -1,8 +1,3 @@
--- ########################################################
--- # Maintainer: Mr. Charkuils                            #
--- # URL:        https://github.com/charkuils/nvim-whisky #
--- ########################################################
-
 local util = require 'whisky.util'
 local linkAll = util.linkAll
 
