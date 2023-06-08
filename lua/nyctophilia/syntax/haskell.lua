1d5455a688261ad683de54cf3245fdcde5e99b84
Javier Orfo <javierorfo@protonmail.com>
2023-06-08 16:50:42 -0300

    haskell infix syntax

--- lua/nyctophilia/syntax/haskell.lua
+++ lua/nyctophilia/syntax/haskell.lua
@@ -2,6 +2,7 @@ local linkAll = require'nyctophilia.util'.linkAll
 
 linkAll {
     hsImport    = "Type",
+    hsInfix     = "Type",
     hsStructure = "hsImport",
     hsImportMod = "hsImport",
     hsDelimiter = "hsImport",
