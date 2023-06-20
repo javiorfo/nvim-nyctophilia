0542dcab02647285f0499488aefeff6cdf89909d
Javier Orfo <javierorfo@protonmail.com>
2023-06-20 20:43:49 -0300

    go types signs syntax

--- lua/nyctophilia/syntax/go.lua
+++ lua/nyctophilia/syntax/go.lua
@@ -1,6 +1,15 @@
 local linkAll = require'nyctophilia.util'.linkAll
 
 linkAll {
-    goDeclaration = "Type",
-    goVar         = "Type"
+    goDeclaration  = "Type",
+    goVar          = "Type",
+    goBlock        = "Type",
+    goOperator     = "Type",
+    goType         = "Boolean",
+    goSignedInts   = "goType",
+    goUnsignedInts = "goType",
+    goFloats       = "goType",
+    goComplexes    = "goType"
 }
+
+vim.g.go_highlight_operators = 1
