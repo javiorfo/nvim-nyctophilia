6841ac0450cb699c908a0d1fc7c2d895f266f767
Javier Orfo <javierorfo@protonmail.com>
2023-07-03 17:18:28 -0300

    zig syntax change

--- lua/nyctophilia/syntax/zig.lua
+++ lua/nyctophilia/syntax/zig.lua
@@ -1,15 +1,16 @@
 local linkAll = require 'nyctophilia.util'.linkAll
 
 linkAll {
-    zigBoolean   = "Boolean",
-    zigNull      = "zigType",
-    zigStructure = "Type",
-    zigExecution = "Type",
-    zigKeyword   = "Type",
-    zigPreProc   = "Type",
-    zigBraces    = "Type",
-    zigVarDecl   = "Type",
-    zigBuiltinFn = "Annotation",
-    zigMacro     = "Tag",
-    zigType      = "Boolean"
+    zigBoolean        = "Boolean",
+    zigNull           = "zigType",
+    zigStructure      = "Type",
+    zigExecution      = "Type",
+    zigComparatorWord = "Type",
+    zigKeyword        = "Type",
+    zigPreProc        = "Type",
+    zigBraces         = "Type",
+    zigVarDecl        = "Type",
+    zigBuiltinFn      = "Annotation",
+    zigMacro          = "Tag",
+    zigType           = "Boolean"
 }
