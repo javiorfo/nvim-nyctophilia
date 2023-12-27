c8afb7428dc854bf56c9447c4ba3e7be2300a789
Javier Orfo <javierorfo@prontonmail.com>
2023-12-26 23:10:15 -0300

    haskell syntax changes

--- lua/nyctophilia/syntax/haskell.lua
+++ lua/nyctophilia/syntax/haskell.lua
@@ -1,10 +1,13 @@
 local linkAll = require'nyctophilia.util'.linkAll
 
 linkAll {
-    hsImport    = "Type",
-    hsInfix     = "Type",
-    hsStructure = "hsImport",
-    hsImportMod = "hsImport",
-    hsDelimiter = "hsImport",
-    hsPragma    = "Annotation"
+    hsImport     = "Type",
+    hsInfix      = "Type",
+    hsStructure  = "hsImport",
+    hsImportMod  = "hsImport",
+    hsDelimiter  = "hsImport",
+    hsPragma     = "Annotation",
+    hsTypedef    = "Type",
+    hsNewTypedef = "Type",
+    hsTypeFam    = "Type",
 }
